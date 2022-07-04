c005775ada130fbb6ba98b529375f13844812480
ChoiChangSeo <navida31245@gmail.com>
2022-07-04 15:16:56 +0900

    hot fix

--- src/component/units/Reply/ReplyContainer.jsx
+++ src/component/units/Reply/ReplyContainer.jsx
@@ -9,25 +9,20 @@ export default function ReplyPage(props) {
   const onClickSubmit = async (e) => {
     e.preventDefault();
     setReply([...reply, SubmitRef.current?.value]);
-    const result = await axios.put(
-      `http://localhost:4000/posts/${props.el.id}`,
-      {
-        like: props.el.like,
-        image: props.el.image,
-        writer: props.el.writer,
-        title: props.el.title,
-        reply: [...reply, SubmitRef.current?.value],
-      }
-    );
+    await axios.put(`http://localhost:4000/posts/${props.el.id}`, {
+      like: props.el.like,
+      image: props.el.image,
+      writer: props.el.writer,
+      title: props.el.title,
+      reply: [...reply, SubmitRef.current?.value],
+    });
     SubmitRef.current.value = '';
   };
 
   useEffect(() => {
-    const result = axios
-      .get(`http://localhost:4000/posts/${props.el.id}`)
-      .then((res) => {
-        setReply(res.data.reply);
-      });
+    axios.get(`http://localhost:4000/posts/${props.el.id}`).then((res) => {
+      setReply(res.data.reply);
+    });
   }, []);
 
   return (
