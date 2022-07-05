78d6d68effb82cdd3aa35a57e209a557374be428
seongjin <77876601+ysjkof@users.noreply.github.com>
2022-07-05 15:33:05 +0900

    모바일 페이지 하단 네비게이션 로그아웃 버튼 온클릭 연결
    #10

--- src/component/commons/Navigation/SubHeader/SubHeaderContainer.jsx
+++ src/component/commons/Navigation/SubHeader/SubHeaderContainer.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import SubHeaderUI from './SubHeaderPresenter';
 
 export default function SubHeader() {
-  return <SubHeaderUI />;
+  let navigate = useNavigate();
+  const Logout = () => {
+    localStorage.removeItem('LoginUser');
+    navigate('/');
+  };
+  return <SubHeaderUI Logout={Logout} />;
 }
