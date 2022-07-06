a4b8dd0ec451c8de8fe705339a23af5ffb78f0cd
Avoler0 <jys1391@gmail.com>
2022-07-06 11:03:36 +0900

    오버레이 컴포넌트 분할

--- src/component/units/Main/MainFeed.jsx
+++ src/component/units/Main/MainFeed.jsx
@@ -3,32 +3,61 @@ import * as S from './MainStyles';
 import Reply from '../Reply/ReplyContainer';
 import FeedDataService from '../../../services/DataService';
 import ReplyOverlay from '../Reply/ReplyOverlay';
-import axios from 'axios';
+import { getLoginUser } from '../../commons/utils/lib';
 
-export default function MainFeed({ board, loading, setLike, setLoading }) {
+export default function MainFeed(props) {
+  const { board, loading, setLike, setLoading } = props;
   const { id, image, like, reply, title, writer } = board;
+
   const [showReplyOverlay, setShowReplyOverlay] = useState(false);
-  
+  const hasLike = getLoginUser().like[id];
   return (
-    <S.Wrapper load={loading} key={id}>
+    <S.Wrapper load={props.loading} key={id}>
       <S.HeadWrapper>
         <S.UserImg></S.UserImg>
         <S.Head>{writer}</S.Head>
       </S.HeadWrapper>
-      <S.MainImg src={image} onLoad={() => setLoading?.(true)} />
+      <S.MainImg src={image} onLoad={() => props.setLoading?.(true)} />
       <S.ButtonWrapper>
         <S.LikeImg
           id={id}
+          hasLike={hasLike}
           onClick={async () => {
-            await FeedDataService.updateFeed({
-              ...board,
-              like: like + 1,
-            }).then((res) => {
-              setLike((prev) => !prev);
+            await FeedDataService.toggleLike(props.board).then((res) => {
+              props.setLike((prev) => !prev);
             });
           }}
-          src="/Header/heart.png"
-        />
+        >
+          {hasLike ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
+                clipRule="evenodd"
+              />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+              />
+            </svg>
+          )}
+        </S.LikeImg>
         <S.CommentImg src="/comment.png" />
         <S.MsgImg src="/Header/send.png" />
       </S.ButtonWrapper>
@@ -37,15 +66,13 @@ export default function MainFeed({ board, loading, setLike, setLoading }) {
         댓글 모두 보기 : {id}
       </div>
       <Reply el={board} />
-      <div>
-        {showReplyOverlay && (
-          <ReplyOverlay
-            data={board}
-            show={showReplyOverlay}
-            setShow={setShowReplyOverlay}
-          />
-        )}
-      </div>
+      {showReplyOverlay && (
+        <ReplyOverlay
+          data={board}
+          show={showReplyOverlay}
+          setShow={setShowReplyOverlay}
+        />
+      )}
     </S.Wrapper>
   );
 }
--- src/component/units/Main/MainPresenter.jsx
+++ src/component/units/Main/MainPresenter.jsx
@@ -4,82 +4,26 @@ import Reply from '../Reply/ReplyContainer';
 import FeedDataService from '../../../services/DataService';
 import ReplyOverlay from '../Reply/ReplyOverlay';
 import { getLoginUser } from '../../commons/utils/lib';
+import Feed from './MainFeed';
 
 export default function MainPageUI(props) {
   const [showReplyOverlay, setShowReplyOverlay] = useState(false);
 
   return (
-    <>   
+    <>
       {props.board
         .sort((a, b) => b.id - a.id)
         .map((el) => {
-        const hasLike = getLoginUser().like[el.id];
-        return 
-        (
-          <S.Wrapper load={props.loading} key={el.id}>
-            <S.HeadWrapper>
-              <S.UserImg></S.UserImg>
-              <S.Head>{el.writer}</S.Head>
-            </S.HeadWrapper>
-            <S.MainImg src={el.image} onLoad={() => props.setLoading?.(true)} />
-            <S.ButtonWrapper>
-              <S.LikeImg
-                id={el.id}
-                hasLike={hasLike}
-                onClick={async () => {
-                  await FeedDataService.toggleLike(el).then((res) => {
-                    props.setLike((prev) => !prev);
-                  });
-                }}
-              >
-                {hasLike ? (
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-5 w-5"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                ) : (
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-6 w-6"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeWidth={2}
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-                    />
-                  </svg>
-                )}
-              </S.LikeImg>
-              <S.CommentImg src="/comment.png" />
-              <S.MsgImg src="/Header/send.png" />
-            </S.ButtonWrapper>
-            <S.Like>좋아요 {el.like}</S.Like>
-            <div onClick={() => setShowReplyOverlay((prev) => !prev)}>
-              댓글 모두 보기 : {el.id}
-            </div>
-            <Reply el={el} />
-            {showReplyOverlay && (
-              <ReplyOverlay
-                data={el}
-                show={showReplyOverlay}
-                setShow={setShowReplyOverlay}
-              />
-            )}
-          </S.Wrapper>
-        )};
-      })}
+          return (
+            <Feed
+              board={el}
+              loading={props.loading}
+              setLike={props.setLike}
+              setLoading={props.setLoading}
+              key={el.id}
+            />
+          );
+        })}
     </>
   );
 }
